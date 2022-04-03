fcaf63a95ec04ab8ef0f51465f2aaa059038a194
ZsgsDesign <zsgsdesign@gmail.com>
2022-04-04 00:43:49 +0800

    add ca-certificates installization

--- tools/linux/automate.py
+++ tools/linux/automate.py
@@ -179,6 +179,8 @@ build_tools_params = ["--branch", branch,
                       "--qt-dir", os.getcwd() + "/qt_build/Qt-5.9.9"]
 
 base.cmd("git", ["config", "--global", "http.sslVerify", "false"])
+base.cmd("sudo", ["apt-get", "install", "ca-certificates"])
+base.cmd("echo", ["\"insecure\"", ">>",  "\"~/.curlrc\""])
 
 base.cmd_in_dir("../..", "./configure.py", build_tools_params)
 base.cmd_in_dir("../..", "./make.py")
